192ab45b2e7698d2bde59109dfdf273e8f4087c7
Harsh Dwivedi <harshdvd10@gmail.com>
2022-04-12 01:33:02 +0530

    Remove Duplicates from sorted array

---
+++ src/Remove_Duplicates_from_sorted_array.java
@@ -0,0 +1,14 @@
+class Remove_Duplicates_from_sorted_array {
+//        public int removeDuplicates(int[] nums) {
+//            int j=0;
+//            int last_unique=-101;
+//            for(int i=0;i<nums.length;i++){
+//                if(nums[i]!=last_unique){
+//                    last_unique=nums[i];
+//                    nums[j]=nums[i];
+//                    j++;
+//                }
+//            }
+//            return j;
+//        }
+}
